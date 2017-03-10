166b041384e504acc3dd5e25aa3726a8c99d2c2f
kimsora <kimsora@ohjic.com>
2017-03-10 10:50:27 +0900

    initial version'

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ohjic on 2017-02-20.
+ */
+import React from 'react';
+
+class App  extends React.Component{
+    render(){
+        return (
+            <h1>hello</h1>
+        );
+
+    }
+
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ohjic on 2017-02-20.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement =  document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
