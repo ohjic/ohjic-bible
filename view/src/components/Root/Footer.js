ec992cc675e0633a3c194162d602522c98f9d3c8
aros7 <k910715@gmail.com>
2017-03-23 00:40:18 +0900

    commit

--- view/src/components/Root/Footer.js
+++ view/src/components/Root/Footer.js
@@ -3,7 +3,7 @@
  */
 import React, {Component} from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Link, Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import SiderBar from './Sidebar';
@@ -13,7 +13,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="menu l-footer">
-                <SiderBar name="성경"></SiderBar>
+                <SiderBar name="성경" component={SiderBar}></SiderBar>
                 <SiderBar name="설정"></SiderBar>
             </div>
         )
--- view/src/components/Root/Sidebar.js
+++ view/src/components/Root/Sidebar.js
@@ -1,20 +1,42 @@
 /**
  * Created by ksb on 17. 1. 29.
  */
-import React, {Component} from 'react';
-import {Row, Col, Image} from 'react-bootstrap';
+import React, {Component, PropTypes} from 'react';
 import { Link } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default class SiderBar extends Component {
 
+    constructor(props){
+        super(props);
+    }
+
     render() {
+        const mapToComponent = ()=>{
+            if(this.props.iconName == ''){
+                return (<span>{this.props.name}</span>);
+            } else {
+                return (<i className={this.props.iconName}></i>);
+            }
+        };
+
         return (
-            <div className="menu-item">
-                <i className={this.props.iconName}></i>
+            <div className="menu-item" >
+                {mapToComponent()}
             </div>
         )
     }
 }
+
+SiderBar.defaultProps = {
+    iconName : '',
+    name : ''
+
+};
+
+SiderBar.propTypes = {
+    iconName : PropTypes.string,
+    name : PropTypes.string
+};
 
 
